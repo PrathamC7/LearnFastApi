bf882c04cc063c31a58bbddf7c9af4861ec4dbe9
User <prathamc790@gmail.com>
2026-09-12 10:23:53 +0530

    changed the port

--- fastapi-demo/04-products-with-ui/frontend/src/App.js
+++ fastapi-demo/04-products-with-ui/frontend/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import TaglineSection from "./TaglineSection";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+    baseURL: "http://15.206.90.28:8000",
 });
 
 function App() {
